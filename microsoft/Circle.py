d5a06a8a83a619c013d1a8b8782b43534b94b248
DavidBestaev <lazyentity@list.ru>
2022-12-29 16:05:34 +0700

    k

--- microsoft/Circle.py
+++ microsoft/Circle.py
@@ -1,17 +1,15 @@
 class Solution:
     def solution(self, arr):
-        idx = 0
-        result = 0
-        while idx < len(arr) - 1:
+        idx, size, result = 0, len(arr), 0
+        if (arr[0] + arr[-1]) % 2 == 0:
+            idx, size, result = 1, size - 1, 1
+        while idx < size - 1:
             if (arr[idx] + arr[idx + 1]) % 2 == 0:
                 result += 1
                 idx += 1
             idx += 1
-
-        if idx == len(arr) - 1 and (arr[0] + arr[-1]) % 2 == 0:
-            result += 1
         return result
 
 
 if __name__ == '__main__':
-    print(Solution().solution([5, 5, 5, 5, 5, 5]))
+    print(Solution().solution([5, 5, 5]))
